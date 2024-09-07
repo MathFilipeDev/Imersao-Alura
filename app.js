27f890813fa3f85ee9829614e9b9cbc181253753
Matheus Filipe <math.filipe23@gmail.com>
2024-09-06 23:46:53 -0300

    [feat] change-javascript

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-// console.log(dados);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
 
-let section = document.getElementById("resultados-pesquisa")
-console.log(section);
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+// se campoPesquisa for uma string sem nada
+if (campoPesquisa == "") {
+    section.innerHTML = "<p>Nada foi encontrado</p>" 
+    return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+console.log(campoPesquisa);
+
+let resultados = "";
+let titulo = "";
+let descricao = "";
 
 for (let dado of dados) {
-    section.innerHTML += `
-    <div class="item-resultado">
-        <img src="${dado.img}">
-            <h2>${dado.titulo}</h2>
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+
+        resultados += `
+            <div class="item-resultado">
+             <img src="${dado.img}">
+              <h2>${dado.titulo}</h2>
                 <p class="descricao-meta">${dado.descricao}</p>
             <a href="${dado.link}" target="_blank">Mais Informações</a>
     </div>
 `
+    }
+
+}
+
+section.innerHTML = resultados
 }
--- dados.js
+++ dados.js
@@ -3,54 +3,63 @@ let dados = [
         titulo: "Super Mario Bros",
         descricao: "Um dos jogos mais icônicos de todos os tempos e introduziu personagens inesquecíveis como Mario e Luigi.",
         img: "img/mario.jpg", // Certifique-se que o caminho esteja correto
-        link: "https://pt.wikipedia.org/wiki/Super_Mario_Bros"
+        link: "https://pt.wikipedia.org/wiki/Super_Mario_Bros",
+        tags: "mario cano arcade luide"
       },
       {
         titulo: "The Legend of Zelda",
         descricao: "Um marco nos jogos de aventura, com um mundo aberto, puzzles desafiadores e uma história envolvente.",
         img: "img/zelda.jpg",
-        link: "https://pt.wikipedia.org/wiki/The_Legend_of_Zelda:_A_Link_to_the_Past"
+        link: "https://pt.wikipedia.org/wiki/The_Legend_of_Zelda:_A_Link_to_the_Past",
+        tags: ""
       },
       {
         titulo: "Final Fantasy VII",
         descricao: "Um dos RPGs mais aclamados, com uma narrativa épica, personagens memoráveis e um sistema de combate inovador.",
         img: "img/final-fantasy.jpg",
-        link: "https://pt.wikipedia.org/wiki/Final_Fantasy_VII"
+        link: "https://pt.wikipedia.org/wiki/Final_Fantasy_VII",
+        tags: ""
       },
       {
         titulo: "Metal Gear Solid",
         descricao: "Um dos pioneiros dos jogos de espionagem, com uma jogabilidade tensa e personagens carismáticos.",
         img: "img/metal-gear.jpg",
-        link: "https://pt.wikipedia.org/wiki/Metal_Gear_Solid"
+        link: "https://pt.wikipedia.org/wiki/Metal_Gear_Solid",
+        tags: ""
       },
       {
         titulo: "Tony Hawk's Pro Skater 2",
         descricao: "Um clássico dos jogos de skate, que popularizou o gênero e inspirou gerações de skatistas virtuais.",
         img: "img/tony-hawk.jpg",
-        link: "https://pt.wikipedia.org/wiki/Tony_Hawk%27s_Pro_Skater_2"
+        link: "https://pt.wikipedia.org/wiki/Tony_Hawk%27s_Pro_Skater_2",
+        tags: ""
       },
       {
         titulo: "The Sims",
         descricao: "Um simulador de vida que permite aos jogadores criar e controlar suas próprias famílias virtuais.",
         img: "img/the-sims.jpg",
-        link: "https://pt.wikipedia.org/wiki/The_Sims"
+        link: "https://pt.wikipedia.org/wiki/The_Sims",
+        tags: ""
       },
       {
         titulo: "Diablo II",
         descricao: "Um RPG de ação viciante, com hordas de inimigos, itens lendários e um sistema de progressão profundo.",
         img: "img/diablo2.jpg",
-        link: "https://en.wikipedia.org/wiki/Diablo_II"
+        link: "https://en.wikipedia.org/wiki/Diablo_II",
+        tags: ""
       },
       {
         titulo: "Grand Theft Auto III",
         descricao: "Um marco nos jogos de mundo aberto, com uma liberdade de exploração sem precedentes e uma narrativa realista.",
         img: "img/gta3.jpg",
-        link: "https://en.wikipedia.org/wiki/Grand_Theft_Auto_III"
+        link: "https://en.wikipedia.org/wiki/Grand_Theft_Auto_III",
+        tags: ""
       },
       {
         titulo: "Resident Evil 2",
         descricao: "Um dos jogos de terror mais icônicos de todos os tempos, com uma atmosfera tensa e zumbis assustadores.",
         img: "img/resident-evil2.jpg",
-        link: "https://en.wikipedia.org/wiki/Resident_Evil_2"
+        link: "https://en.wikipedia.org/wiki/Resident_Evil_2",
+        tags: ""
       }
     ];
